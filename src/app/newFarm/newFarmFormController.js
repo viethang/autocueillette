b9dab589860fb135d54afb814851df952562a9b9
Viet Hang Nguyen <viethangybvn@gmail.com>
2015-08-13 10:30:32 +0200

    Modified NewFarmFormController to send data to server.

--- src/app/newFarm/newFarmFormController.js
+++ src/app/newFarm/newFarmFormController.js
@@ -1,5 +1,32 @@
-app.controller('newFarmFormController', ['$scope', function($scope) {
+app.controller('NewFarmFormController', ['$scope', '$http',
+					function($scope, $http) {
+	$scope.products = [{}];
+
 	this.submit = function() {
+		var farm = {
+			name: this.name,
+			address: {
+				canton: this.canton,
+				city: this.city,
+				postalCode: this.postalCode,
+				street: this.street
+			},
+			tel: this.tel,
+			products: this.products,
+			openHours: this.openHours
+		};
+		var req = {
+			method: 'post',
+			url: '/newFarm.html',
+			data: farm
+		};
+		$http(req).then(function() {
+			console.log('ok');
+			this.name = 'tata';
+			this.city = undefined;
+		}.bind(this), function() {
+			console.log('error');
+		});
 		console.log('submit!');
 	};
 	$scope.cantons = ['Vaud', 'Valais'];
