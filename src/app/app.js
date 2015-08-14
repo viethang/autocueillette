c336baf7c61e6665b513c2c31c9059e071f0c497
Viet Hang Nguyen <viethangybvn@gmail.com>
2015-08-14 17:49:39 +0200

    Route newFarmForm and searchFarmForm with ui-router

--- src/app/app.js
+++ src/app/app.js
@@ -1,1 +1,14 @@
-var app = angular.module('app',[]);
+var app = angular.module('app',['ui.router', 'templates-app']);
+app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
+	$locationProvider.html5Mode(true);
+	$urlRouterProvider.otherwise('/index.html');
+	$stateProvider
+	.state('newFarm', {
+		url: "/newFarm",
+		templateUrl: "newFarm/newFarmForm.tpl.html"
+	})
+	.state('searchFarm', {
+		url: '/searchFarm',
+		templateUrl: "searchFarm/searchFarmForm.tpl.html"
+	});
+}]);
