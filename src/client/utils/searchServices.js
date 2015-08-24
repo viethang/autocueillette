fcaadc997f1219736b52cca872e2079c0fc63ef8
viethang <viethang@debxps>
2015-08-24 11:02:56 +0200

    Create searchService

---
+++ src/client/utils/searchServices.js
@@ -0,0 +1,23 @@
+function searchService($http, BingKey) {
+	this.bingSearch = function(searchStr, callback) {
+		var request = "http://dev.virtualearth.net/REST/v1/Locations?query=" +
+			encodeURIComponent(searchStr) +
+			"&jsonp=JSON_CALLBACK&key=" + BingKey + 
+			"&mkt=fr-fr";
+		$http.jsonp(request)
+		.success(function(response) {
+			if ((response.resourceSets.length === 1) & 
+				(response.resourceSets[0].resources.length === 0)
+			) {
+				console.log('No result found');
+				callback({status: 'NR'});
+			} else {
+				callback({status: 'OK', result: response.resourceSets});
+		}})
+		.error(function() {
+			callback({status: 'ERR'});
+		});
+	};
+}
+
+searchService.$inject = ['$http', 'BingKey'];
