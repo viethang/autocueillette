8c8a0b8371627868d8ff1dc18e15b68586d839e6
Viet Hang Nguyen <viethangybvn@gmail.com>
2015-09-01 14:37:44 +0200

    Separate routing config to route.js

---
+++ src/client/route.js
@@ -0,0 +1,47 @@
+angular.module('app')
+.config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
+		function($stateProvider, $urlRouterProvider, $locationProvider) {
+	$locationProvider.html5Mode(true);
+	$urlRouterProvider.otherwise('/index.html');
+	$stateProvider
+	.state('index', {
+		url: ''
+	})
+	.state('newFarm', {
+		url: '/newFarm',
+		templateUrl: 'components/newFarm/newFarmForm.tpl.html',
+		controller: function($state) {
+			$state.transitionTo('newFarm.fillAddress');
+		}
+	})
+	.state('newFarm.fillAddress', {
+		views: {
+			'simpleForm': {
+				templateUrl: 'components/newFarm/simpleForm.tpl.html'
+			}
+		}
+	})
+	.state('newFarm.confirmAddress', {
+		views: {
+			'simpleForm': {
+				templateUrl: 'components/newFarm/simpleForm.tpl.html'
+			},
+			'parsedAddress': {
+				templateUrl: 'components/newFarm/parsedAddress.tpl.html'
+			},
+			'map': {
+				templateUrl: 'components/newFarm/map.tpl.html'
+			},
+			'addFarmButton': {
+				template: "<div class = 'text-right'><button ng-click = 'newFarmCtrl.addFarm()'>Add this farm</button>"
+			}
+		}
+	})
+	.state('searchFarm', {
+		url: '/searchFarm',
+		templateUrl: 'components/searchFarm/searchFarmForm.tpl.html'
+	})
+	.state('searchFarm.showFarms', {
+		templateUrl:'components/searchFarm/showFarms.tpl.html'
+	});
+}]);
