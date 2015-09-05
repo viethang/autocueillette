2321716ebaa738c05337f9a4ec96df461d8fdb4c
Viet Hang Nguyen <viethangybvn@gmail.com>
2015-09-05 12:10:56 +0200

    Implement farm address edition

--- src/client/components/farmInfo/farmInfoController.js
+++ src/client/components/farmInfo/farmInfoController.js
@@ -3,16 +3,18 @@
 
 	angular.module('app')
 	.controller('FarmInfoController', farmInfoController);
-	function farmInfoController($state, $stateParams, $http, OSMServices, $timeout) {
+	function farmInfoController($state, $stateParams, $http, OSMServices, $timeout, searchService) {
 		/* jshint validthis: true*/
 		var farmId = $stateParams.farmId;
 		var farmInfoCtrl = this;
 		var map = new OSMServices.OSMMap();
+		var formattedAddress;
 		farmInfoCtrl.farm = {products: [{}]};
 		getFarm(farmId, farmInfoCtrl.farm)
 		.then(function() {
 			$state.go('farmInfo.view');
 			var coordinates = farmInfoCtrl.farm.coordinates;
+			var formattedAddress = farmInfoCtrl.farm.formattedAddress;
 			$timeout(function() {
 				map.map.setTarget('map1');
 				map.resetView(coordinates);
@@ -23,6 +25,7 @@
 		farmInfoCtrl.addProduct = addProduct;
 		farmInfoCtrl.removeProduct = removeProduct;
 		farmInfoCtrl.submitDetails = submitDetails;
+		farmInfoCtrl.save = save;
 
 		function editAddress() {
 			$state.go('farmInfo.editAddress');
@@ -67,6 +70,63 @@
 				console.log(err);
 			});
 		}
+
+		function save() {
+			console.log('saving');
+			if (formattedAddress === farmInfoCtrl.farm.formattedAddress) {
+				update(farmInfoCtrl.farm);
+			} else {
+				var searchStr = farmInfoCtrl.farm.formattedAddress;
+				searchService.bingSearch(searchStr, function(response) {
+					switch (response.status) {
+						case 'NR':
+							console.log('No result found');
+							break;
+						case 'ERR':
+							console.log('Error! Try again.');
+							break;
+						case 'OK':
+							if (response.result.length > 1) {
+								alertMultipleResults(response.result);
+							} else {
+								confirmAddress(response.result[0]);
+							}
+							break;
+					}
+				});
+			}
+		}
+
+		function alertMultipleResults(result) {
+			console.log('Multiple results', result);
+		}
+
+		function confirmAddress(result) {
+			var address = result.address;
+			var farm = farmInfoCtrl.farm;
+			farm.city = address.locality;
+			farm.canton = address.adminDistrict;
+			farm.streetLine = address.addressLine;
+			farm.formattedAddress = address.formattedAddress;
+			farm.coordinates = result.geocodePoints[0].coordinates;
+			map.resetView(farm.coordinates);
+			update(farm);
+		}
+
+		function update(farm) {
+			console.log('updating');
+			var req = {
+				method: 'post',
+				url: '/updateFarm',
+				data: farm
+			};
+			$http(req).then(function(res) {
+				console.log('Farm updated');
+			}, function(err) {
+				console.log('update error');
+			});
+			$state.go('farmInfo.view');
+		}
 	}
-	farmInfoController.$inject = ['$state', '$stateParams', '$http', 'OSMServices', '$timeout'];
+	farmInfoController.$inject = ['$state', '$stateParams', '$http', 'OSMServices', '$timeout', 'searchService'];
 })();
